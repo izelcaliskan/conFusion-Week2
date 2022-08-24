7e5e554e003ead1439569d8ff4f4a47e29b882d2
Izel Ozhan <izelozhan@gmail.com>
2022-08-24 22:07:16 +0300

    SPA Part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -7,8 +7,10 @@ import { AboutComponent } from "../about/about.component";
 import { ContactComponent } from "../contact/contact.component";
 
 export const routes: Routes = [
+    {path:'', redirectTo:'/home', pathMatch:'full'},
     {path:'home', component:HomeComponent},
     {path:'menu', component:MenuComponent},
-    {path:'', redirectTo:'/home', pathMatch:'full'},
     {path:'contactus', component:ContactComponent},
+    {path:'dishdetail/:id',component:DishdetailComponent},
+    
 ];
